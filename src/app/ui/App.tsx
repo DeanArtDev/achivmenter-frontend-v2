39ae9d5aaa9d3ce9378b30862d988b93d47ba521
Dean Korobchenko <deankorobchenko@gmail.com>
2022-09-24 15:51:29 +0700

    app

--- src/app/ui/App.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import '../global-style/global.scss';
-
-const App: FC = () => {
-  return <div />;
-};
-
-export default App;
---
+++ src/app/ui/App/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import '../../global-style/global.scss';
+
+// TODO: FC имеен опциональный children, если он не нужен не используй FC
+const App: FC = () => {
+  const text = "";
+  return <div>Hello</div>;
+};
+
+export default App;
