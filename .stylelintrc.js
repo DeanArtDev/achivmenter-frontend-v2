26330732075fde6ead6c39541ab0b4b7be9fb3e3
Dean Korobchenko <deankorobchenko@gmail.com>
2022-09-25 19:54:52 +0700

    add style lint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+// https://stylelint.io/
+module.exports = {
+  extends: "stylelint-config-standard-scss",
+  rules: {
+    "color-hex-length": "long",
+    "string-quotes": "single",
+    indentation: [2],
+    "selector-class-pattern": null,
+  },
+};
